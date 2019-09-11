bc4fee2adffa2bd0ce160c89c4056d14b9a44f5e
Cuong Desktop <nguyenc72694@gmail.com>
2019-09-10 23:47:39 -0500

    done with today

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -2,5 +2,8 @@ $(document).ready(function(){
     $('.parallax').parallax();
     $('.dropdown-trigger').dropdown();
     $(".dropdown-content>li>a").css("color", "black");
+    $(".card").css("width", "30%");
+    $(".card").css("padding", "0");
+    $(".card").css("margin", "10px");
   });
 
